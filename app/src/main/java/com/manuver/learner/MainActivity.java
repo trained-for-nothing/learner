dbb7663729653fff264a9a7ffe0d8626a35c8663
Manuver Gujjar <manuvergujjar@gmail.com>
2020-04-15 01:13:26 +0530

    scrolling text app

--- app/src/main/java/com/manuver/learner/MainActivity.java
+++ app/src/main/java/com/manuver/learner/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        setContentView(R.layout.activity_main);
     }
 
     public void toast(String msg) {
