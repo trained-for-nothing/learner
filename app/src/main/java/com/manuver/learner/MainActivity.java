0810bf278fcf706f4a1eb09a87d03ddaf40eea39
Manuver Gujjar <manuvergujjar@gmail.com>
2020-04-14 22:48:14 +0530

    init
    Signed-off-by: Manuver Gujjar <manuvergujjar@gmail.com>

---
+++ app/src/main/java/com/manuver/learner/MainActivity.java
@@ -0,0 +1,26 @@
+package com.manuver.learner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.ColorStateList;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Iterator;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+
+    public void toast(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+}
