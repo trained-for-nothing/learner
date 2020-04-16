6f75bf1bbea6e8e50ba15ff94afcd595a1a99e7c
Vishal <dhimanv542@gmail.com>
2020-04-16 18:46:25 +0530

    Coding Challenge( Simple Shopping-list App )

--- app/src/main/java/com/manuver/learner/MainActivity.java
+++ app/src/main/java/com/manuver/learner/MainActivity.java
@@ -1,5 +1,6 @@
 package com.manuver.learner;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,38 +22,73 @@ import java.util.Set;
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
-    private EditText mMessageEditText;
     public static final int TEXT_REQUEST = 1;
-    private TextView replyHeaderTextView;
-    private TextView replyTextView;
+    private  static int counter = 1;
+    private  TextView textView1;
+    private  TextView textView2;
+    private  TextView textView3;
+    private  TextView textView4;
+    private  TextView textView5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mMessageEditText = findViewById(R.id.editText_main);
-        replyHeaderTextView = findViewById(R.id.text_header_reply);
-        replyTextView = findViewById(R.id.text_message_reply);
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+        textView4 = findViewById(R.id.textView4);
+        textView5 = findViewById(R.id.textView5);
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putCharSequence("textView1", textView1.getText());
+        outState.putCharSequence("textView2", textView2.getText());
+        outState.putCharSequence("textView3", textView3.getText());
+        outState.putCharSequence("textView4", textView4.getText());
+        outState.putCharSequence("textView5", textView5.getText());
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        textView1.setText(savedInstanceState.getString("textView1"));
+        textView2.setText(savedInstanceState.getString("textView2"));
+        textView3.setText(savedInstanceState.getString("textView3"));
+        textView4.setText(savedInstanceState.getString("textView4"));
+        textView5.setText(savedInstanceState.getString("textView5"));
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == TEXT_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
-                replyHeaderTextView.setVisibility(View.VISIBLE);
-                replyTextView.setText(reply);
-                replyTextView.setVisibility(View.VISIBLE);
+        Log.d(LOG_TAG, "onActivityResult");
+        if(TEXT_REQUEST == requestCode){
+            if(resultCode == RESULT_OK){
+                String reply;
+                reply = data.getStringExtra(SecondActivity.REPLY);
+                counter = (counter%6);
+                if(counter == 0)
+                    counter = 1;
+                switch (counter){
+                    case 1: textView1.setText(reply); break;
+                    case 2: textView2.setText(reply); break;
+                    case 3: textView3.setText(reply); break;
+                    case 4: textView4.setText(reply); break;
+                    case 5: textView5.setText(reply); break;
+                }
+                counter++;
             }
         }
     }
 
     public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG, "Button clicked!");
         Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-        String message = mMessageEditText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
         startActivityForResult(intent, TEXT_REQUEST);
     }
 
--- app/src/main/java/com/manuver/learner/SecondActivity.java
+++ app/src/main/java/com/manuver/learner/SecondActivity.java
@@ -1,35 +1,30 @@
 package com.manuver.learner;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Button;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
-    private TextView textView;
-    public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLy";
-    private EditText editText;
-
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static String reply;
+    public static final String REPLY = "REPLY";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        textView = findViewById(R.id.text_message);
-        textView.setText(message);
-        editText = findViewById(R.id.editText_sencond);
 
     }
 
-    public void returnReply(View view) {
-        String mReply = editText.getText().toString();
-        Intent replyIntent = new Intent();
-        replyIntent.putExtra(EXTRA_REPLY, mReply);
+    public void replyMain(View view) {
+        Log.d(LOG_TAG, "replyMain");
+        Button btn = (Button) view;
+        reply = btn.getText().toString();
+        Log.d(LOG_TAG, reply);
+        Intent replyIntent = new Intent(getApplicationContext(), MainActivity.class);
+        replyIntent.putExtra(REPLY, reply);
         setResult(RESULT_OK, replyIntent);
         finish();
     }
