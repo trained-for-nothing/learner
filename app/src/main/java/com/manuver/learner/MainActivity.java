56c88317e4aa9f354f205e85f1b2fa99ed98656f
Tanya Garg <tanyagarg998@gmail.com>
2020-04-16 23:09:13 +0530

    implicit intent app

--- app/src/main/java/com/manuver/learner/MainActivity.java
+++ app/src/main/java/com/manuver/learner/MainActivity.java
@@ -1,10 +1,15 @@
 package com.manuver.learner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
 
+import android.content.Intent;
 import android.content.res.ColorStateList;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,13 +19,19 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText mWebEditText, mLocEditText, mShareEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TextView textView = new TextView(getApplicationContext());
+        textView.setText(getIntent().getData().toString());
+        setContentView(textView);
     }
 
     public void toast(String msg) {
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
+
+
 }
