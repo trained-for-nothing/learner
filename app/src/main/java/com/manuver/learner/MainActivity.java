83728784786f698078af7d5ad6c55d0b67735836
Tanya Garg <tanyagarg998@gmail.com>
2020-04-28 22:28:42 +0530

    menus

--- app/src/main/java/com/manuver/learner/MainActivity.java
+++ app/src/main/java/com/manuver/learner/MainActivity.java
@@ -1,32 +1,63 @@
 package com.manuver.learner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ShareCompat;
+import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.net.Uri;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Iterator;
-import java.util.Set;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
-    private EditText mWebEditText, mLocEditText, mShareEditText;
+    private Button TNCButton;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_new:
+                toast("new");
+                return true;
+            case R.id.menu_open:
+                toast("open");
+                return true;
+            case R.id.menu_setting:
+                toast("setting");
+                return true;
+            default:
+                return true;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView textView = new TextView(getApplicationContext());
-        textView.setText(getIntent().getData().toString());
-        setContentView(textView);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TextView simpleTextView = findViewById(R.id.simple_text);
+        registerForContextMenu(simpleTextView);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
     }
 
     public void toast(String msg) {
